c59ae85a30f6ea50ebd808253b3afb63f3432a2b
Noflowerzzk <noflowerzzk@outlook.com>
2021-08-08 14:50:45 +0800

    添加项目文件。

---
+++ Question1/Question1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stack>
+#include <cstdio>
+
+using namespace std;
+
+stack <pair<int, char>> stk;
+// vector <pair<int, int>> vct;
+int n, p;
+
+int main()
+{
+	cin >> n;
+	cin >> p;
+
+	char ch;
+	int output;
+
+	cin.get();
+
+	for (int i = 1; i <= n; i++) {
+		cin.get(ch);
+
+		if (stk.empty()) {
+			stk.push(pair<int, char>{i, ch});
+			continue;
+		}
+		else if (stk.top().second == '{' && ch == '}') {
+
+			if (stk.top().first == p) {
+				// cout << static_cast<char>(i);
+				// printf("%d", i);
+				// return 0;
+				output = i;
+				break;
+			}
+			else if (i == p) {
+				// cout << static_cast<char>(stk.top().first);
+				// printf("%d", stk.top().first);
+				// return 0;
+				output = stk.top().first;
+				break;
+			}
+			else
+				stk.pop();
+		}
+		else
+			stk.push(pair<int, char>{i, ch});
+	}
+
+	cout << output;
+
+
+	return 0;
+}
---
+++ Question4/Question4.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class node {
+public:
+	vector<int> nodes;
+	bool is;
+	int indegree;
+
+	node() :nodes{ vector<int>{} }, is{ false }, indegree{ 0 }{}
+
+	inline void add(int i) {
+		nodes.push_back(i);
+	}
+
+	inline void clear() {
+		nodes.clear();
+		indegree = 0;
+	}
+};
+
+vector<int> list{};
+vector<node> graph{};
+int n;
+
+bool ifeulercircuit(int i, int j) {
+	int od = 0;
+
+
+	for (int ii = i; ii <= j; ii++)
+		for (int jj = ii + 1; jj <= j; jj++) {
+			if (list[ii] < list[jj]) {
+				graph[ii].add(jj);
+				graph[jj].add(ii);
+			}
+		}
+
+	for (int ij = i; ij <= j; ij++) {
+		for (int e : graph[ij].nodes)
+			if (e >= i && e <= j)
+				graph[ij].indegree++;
+		if (graph[ij].indegree % 2 == 1)
+			od++;
+	}
+
+	for (int m = 0; m < n; m++)
+		graph[m].clear();
+
+
+	if (od == 0)
+		return true;
+	else
+		return false;
+}
+
+
+int main()
+{
+	cin >> n;
+
+	list.resize(n);
+	graph.resize(n);
+	cin.get();
+
+	for (int i = 0; i < n; i++)
+		cin >> list[i];
+
+	//for (int i = 0; i < n; i++)
+	//	for (int j = i + 1; j < n; j++) {
+	//		if (list[i] < list[j]) {
+	//			graph[i].add(j);
+	//			graph[j].add(i);
+	//		}
+	//	}
+
+	int ngood = 0;
+
+	for (int i = 0; i < n; i++)
+		for (int j = i; j < n; j++) {
+			if (i == j)
+				ngood++;
+			else if (ifeulercircuit(i, j))
+				ngood++;
+		};
+
+	cout << ngood;
+	
+	return 0;
+}
